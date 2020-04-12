aac530a892033fe932e54ff3f3c85d225199ca8a
brunubarbosa <brunopjsw@gmail.com>
2020-04-12 04:36:13 -0300

    base ring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const myArc = (cx, cy, radius, max) => {
+    var d = " M " + (cx + radius) + " " + cy;
+    let ang = 0;
+    while (ang <= max) {
+      var radians = ang * (Math.PI / 180); // convert degree to radians
+      var x = cx + Math.cos(radians) * radius;
+      var y = cy + Math.sin(radians) * radius;
+      d += " L " + x + " " + y;
+      ang++;
+    }
+    return d;
+  };
+
+  const dd = (amount) =>
+    myArc(
+      35,
+      35,
+      30,
+      parseInt(360 / amount) == 360 ? 360 : parseInt(360 / amount) - 10
+    );
+  const amount = [1, 2, 3, 4, 5, 6, 7, 7, 9, 10];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg width="70" height="70">
+        {amount.map((item, index) => (
+          <path
+            d={dd(amount.length)}
+            fill="none"
+            stroke={"#b5b5b5"}
+            strokeWidth={7}
+            // transform={{
+            //   rotation: (360 / 5) * index + 1,
+            //   originX: 35,
+            //   originY: 35,
+            // }}
+            rx={0}
+            transform={`rotate(${(360 / amount.length) * index + 1} 35 35)`}
+          />
+        ))}
+      </svg>
     </div>
   );
 }
