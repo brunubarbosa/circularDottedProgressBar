f35af1b3a993817091cc8d68022951f2df9192ce
brunubarbosa <brunopjsw@gmail.com>
2020-04-12 05:48:57 -0300

    template literals

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const myArc = (cx, cy, radius, amount) => {
-    var d = " M " + (cx + radius) + " " + cy;
+  const getDAttribute = (cx, cy, radius, amount) => {
+    var d = ` M ${cx + radius} ${cy}`;
     let ang = 0;
     while (ang <= amount) {
       var radians = ang * (Math.PI / 180); // convert degree to radians
       var x = cx + Math.cos(radians) * radius;
       var y = cy + Math.sin(radians) * radius;
-      d += " L " + x + " " + y;
+      d += ` L ${x} ${y}`;
       ang++;
     }
     return d;
   };
 
   const dd = (amount) =>
-    myArc(
+    getDAttribute(
       35,
       35,
-      30,
+      50,
       parseInt(360 / amount) == 360 ? 360 : parseInt(360 / amount) - 10
     );
   const amount = [1, 2, 3, 4, 5, 6, 7, 7, 9, 10];
@@ -28,19 +28,13 @@ function App() {
   const active = 50;
   return (
     <div className="App">
-      <svg width="70" height="70">
+      <svg width="100" height="100">
         {amount.map((item, index) => (
           <path
             d={dd(amount.length)}
             fill="none"
-            stroke={index + 1 > 5 ? "#b5b5b5" : "red"}
-            strokeWidth={7}
-            // transform={{
-            //   rotation: (360 / 5) * index + 1,
-            //   originX: 35,
-            //   originY: 35,
-            // }}
-            rx={0}
+            stroke={index + 1 > 8 ? "#b5b5b5" : "red"}
+            strokeWidth={15}
             transform={`rotate(${
               (360 / amount.length) * index + 1 + 270
             } 35 35)`}
